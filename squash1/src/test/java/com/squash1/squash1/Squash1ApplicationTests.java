20d571e4b2bf2bbc3c68c325c075e453eeb5886b
Bartek <durczokbartlomiej@gmail.com>
2023-10-19 12:09:43 +0200

    podstawowa wersja

---
+++ squash1/src/test/java/com/squash1/squash1/Squash1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squash1.squash1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Squash1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
