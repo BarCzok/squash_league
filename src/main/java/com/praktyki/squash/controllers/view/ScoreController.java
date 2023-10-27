1829b6f06cd2b40ed9032813969c97e96cfe52ce
Bartek <durczokbartlomiej@gmail.com>
2023-10-27 12:09:58 +0200

    zapis do bazy

--- src/main/java/com/praktyki/squash/controllers/view/ScoreController.java
+++ src/main/java/com/praktyki/squash/controllers/view/ScoreController.java
@@ -2,9 +2,12 @@ package com.praktyki.squash.controllers.view;
 
 import com.praktyki.squash.controllers.forms.ScoreForm;
 import com.praktyki.squash.facades.GameFacade;
+import com.praktyki.squash.facades.ScoreFacade;
 import com.praktyki.squash.facades.dto.GameDTO;
 import com.praktyki.squash.facades.dto.PlayerDTO;
 import com.praktyki.squash.model.Game;
+import com.praktyki.squash.model.Score;
+import com.praktyki.squash.repository.ScoreRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +21,12 @@ public class ScoreController {
     @Resource
     GameFacade gameFacade;
 
+    @Resource
+    ScoreFacade scoreFacade;
+
+    @Resource
+    ScoreRepository scoreRepository;
+
     @GetMapping(value = "score/{gameId}")
     public String score(@PathVariable Integer gameId, ModelMap model){
 
@@ -29,7 +38,14 @@ public class ScoreController {
     }
     @PostMapping(value = "/addScore")
     public String addScore(@ModelAttribute ScoreForm score){
-//        scoreFacade.addScore(score);
+
+        Score player1Score = new Score();
+        player1Score.setSets(score.getPoints1());
+        scoreRepository.save(player1Score);
+
+        Score player2Score = new Score();
+        player2Score.setSets(score.getPoints2());
+        scoreRepository.save(player2Score);
 
         return "redirect:/games/view";
     }
