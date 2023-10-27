88b671479b51378dd5276be1d98d7e2f8d104560
wojtek <wojciechmrozek321@gmail.com>
2023-10-27 11:42:08 +0200

    usuniecie obiektow z repozytorium

--- src/main/java/com/praktyki/squash/controllers/forms/ScoreForm.java
+++ src/main/java/com/praktyki/squash/controllers/forms/ScoreForm.java
@@ -3,6 +3,24 @@ package com.praktyki.squash.controllers.forms;
 public class ScoreForm {
     int points1;
     int points2;
+    String player1Name;
+    String player2Name;
+    int gameId;
+    public String getPlayer1Name() {
+        return player1Name;
+    }
+
+    public void setPlayer1Name(String player1Name) {
+        this.player1Name = player1Name;
+    }
+
+    public String getPlayer2Name() {
+        return player2Name;
+    }
+
+    public void setPlayer2Name(String player2Name) {
+        this.player2Name = player2Name;
+    }
 
     public int getPoints1() {
         return points1;
